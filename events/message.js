6a6c93ab41d7aa2dc797cd350b9eec2e9cb3c95d
FlamyEdge <potato@DESKTOP-EVF5QUT.localdomain>
2019-04-11 16:36:20 +0200

    Embed test

--- events/message.js
+++ events/message.js
@@ -1,6 +1,7 @@
 const kick = require('../commands/kick')
 const embed = require('../commands/embed')
 module.exports = (client, message) => {
+  if (message.author.bot) return
   if (message.content.startsWith('!kick')) {
     return kick(message)
   }
