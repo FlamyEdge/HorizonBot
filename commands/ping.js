33fe7e29e1ceed65debb2a425ee8de2406fc78d4
FlamyEdge <potato@DESKTOP-EVF5QUT.localdomain>
2019-04-12 11:13:24 +0200

    ping command add

--- commands/ping.js
+++ commands/ping.js
@@ -3,5 +3,5 @@ module.exports = async message => {
   const client = new Discord.Client();
 
   const m = await message.channel.send("Ping?");
-  m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms.`);
 }
