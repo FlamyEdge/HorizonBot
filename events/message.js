8ce6da094af3538617af4add4b2b5c4c16390916
FlamyEdge <potato@DESKTOP-EVF5QUT.localdomain>
2019-04-12 10:48:38 +0200

    ping command add

--- events/message.js
+++ events/message.js
@@ -1,5 +1,6 @@
 const kick = require('../commands/kick')
 const embed = require('../commands/embed')
+const ping = require('../commands/ping')
 module.exports = (client, message) => {
   if (message.author.bot) return
   if (message.content.startsWith('!kick')) {
@@ -7,5 +8,8 @@ module.exports = (client, message) => {
   }
   if (message.content.startsWith('!embed')) {
     return embed(message)
+  }
+  if (message.content.startsWith('!ping')) {
+    return ping(message)
   }
 }
