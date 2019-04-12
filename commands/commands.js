34b49fd8b769e6a681164d3ee039b4cca505a109
FlamyEdge <potato@DESKTOP-EVF5QUT.localdomain>
2019-04-12 18:15:11 +0200

    help command add

--- commands/commands.js
+++ commands/commands.js
@@ -1,18 +1,13 @@
 module.exports = message => {
-  message.reply("Check your dm for the result...")
-  .then(msg => {
-    msg.delete(5000)
-  })
-  .catch(O_o=>{})
-  return message.author.send({embed: {
+  return message.reply({embed: {
     color: "16744448",
-    title: "COMMAND LIST",
+    title: "HorizonBot command list",
     fields: [
         { name: "Command", value: "`!help`\n`!ping`\n`!embed`\n`!addreaction`\n`!deletereaction`\n`!reactions`", inline: true},
-        { name: "Parameter", value: "-\n-\n[color] [message]\n[#room] [message_id] [emoji] [role]\n[reaction_id]\n-", inline: true}
+        { name: "Parameter/s", value: "-\n-\n`[color] [message]`\n`[#room] [message_id] [emoji] [role]`\n`[reaction_id]`\n-", inline: true}
     ],
     footer: {
-		  text: '*Tips: use ?[commands] to get the info about the usage of the passed command.*'
+		  text: 'Tip: use `?[commands]` to get the info about the usage of the passed command'
 	  }
     }})
 }
