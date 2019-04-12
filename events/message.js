9c70ae7e205a5fc0155daebf85834adbc20eccb4
FlamyEdge <potato@DESKTOP-EVF5QUT.localdomain>
2019-04-12 18:10:47 +0200

    help command add

--- events/message.js
+++ events/message.js
@@ -2,6 +2,7 @@ const kick = require('../commands/kick')
 const embed = require('../commands/embed')
 const ping = require('../commands/ping')
 const help = require('../commands/help')
+const commands = require('../commands/commands')
 module.exports = (client, message) => {
   if (message.author.bot) return
   if (message.content.startsWith('!kick')) {
@@ -15,5 +16,8 @@ module.exports = (client, message) => {
   }
   if (message.content.startsWith('!help')) {
     return help(message)
+  }
+  if (message.content.startsWith('!commands')) {
+    return commands(message)
   }
 }
