7d67e50861475980fd18cbea95c1325f9e46bd86
FlamyEdge <potato@DESKTOP-EVF5QUT.localdomain>
2019-04-12 11:03:00 +0200

    ping command add

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
-module.exports = message => {
+module.exports = async message => {
   const m = await message.channel.send("Ping?");
   m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
 }
